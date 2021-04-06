91365f5431db859f2795d655d471773c29ad4ef7
Daniel Hellstern <dhellstern@outlook.com>
2021-04-06 02:24:49 +0000

    Removed authentication components since HTTP will be in use anyway

--- boot.php
+++ boot.php
@@ -10,27 +10,12 @@
     # Ensure that data is only sent as plaintext
     header("Content-Type: text/plain");
 
-    # Load either the preboot script or the menu
-    if (isset($_GET["mac"]) ) {
-        if (isset($_SERVER["PHP_AUTH_USER"]) && isset($_SERVER["PHP_AUTH_PW"])) {
-            # Extract the username and password
-            $username = $_SERVER["PHP_AUTH_USER"];
-            $password = $_SERVER["PHP_AUTH_PW"];
+    if (isset($_GET["mac"])) {
+        log("New iPXE client with MAC address " . $_GET["mac"]);
 
-            # TODO Check if the credentials are valid
-            if (true) {
-                # Present the boot menu
-                include "chain/menu.php";
-            } else {
-                $error = "Invalid username or password";
-                include "chain/preboot.php";
-            }
-        } else {
-            $error = "No credentials supplied";
-            include "chain/preboot.php";
-        }
+        include "chain/menu.php";
     } else {
-        # First redirect to a preboot script which collects and sends client metadata
+        # Show the preboot menu
         include "chain/preboot.php";
     }
 ?>
--- chain/preboot.php
+++ chain/preboot.php
@@ -1,8 +1,3 @@
 #!ipxe
 
-<? if (isset($error)) echo "echo " . $error; ?>
-
-login || goto quit
-chain <?= $proto ?>://\${username:uristring}:\${password:uristring}@<?= $host ?>/boot.php?mac=\${netX/max}
-
-:quit
+chain <?= $proto ?>://<?= $host ?>/boot.php?mac=\${netX/max}
