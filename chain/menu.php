d82980e6431b8ab33760c7909ed10f56ca81de3a
Daniel Hellstern <dhellstern@outlook.com>
2021-04-06 04:16:07 +0000

    Fixed quoting

--- chain/menu.php
+++ chain/menu.php
@@ -32,8 +32,8 @@ item shell iPXE Shell
 choose option && goto ${option}
 
 :search
-echo -n "Enter a search term" && read query
-chain "<?= $proto ?>://<?= $host ?>/boot.php?query=${query:uristring}"
+echo -n Enter a search term:  && read query
+chain <?= $proto ?>://<?= $host ?>/boot.php?query=${query:uristring}
 <?
     # Loop through the operating systems and create a chainloader configuration
     for ($i = 0; $i < count($imageFiles); $i++) {
