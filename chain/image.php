3e68b85868857824087c414cfae01e150c41327c
Daniel Hellstern <dhellstern@outlook.com>
2021-04-06 06:36:04 +0000

    Added ISO file extraction using 7zip

--- chain/image.php
+++ chain/image.php
@@ -1,21 +1,37 @@
 <?php
     # TODO Determine the image type
 
+    function streamRaw($file) {
+        # Get the image MIME type
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $file);
+
+        # Send the content type (overrides top-level type)
+        header("Content-Type: $mimeType");
+
+        # Stream the file contents
+        $fpointer = fopen($file, "rb");
+        fpassthru($fpointer);
+    }
+
     $imageFile = rtrim($imageStore, "/") . "/" . $image;
 
-    # If the instruction is to stream an ISO to the client, then do so instead of chainloading
-    if (isset($_GET["file"])) {
+    # If the instruction is to extract a file from the image, then do so instead of chainloading
+    if ((isset($_GET["file"]) || isset($_GET["raw"]))) {
         if (file_exists($imageFile)) {
-            # Get the image MIME type
-            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            $mimeType = finfo_file($finfo, $imageFile);
+            if (isset($_GET["file"])) {
+                $file = escapeshellarg($_GET["file"]);
+                $tmpFile = uniqid();
 
-            # Send the content type (overrides top-level type)
-            header("Content-Type: $mimeType");
+                # Run 7zip to extract files from the ISO to a temporary location
+                exec("7z x $imageFile -o /tmp/$tmpFile \"$file\"");
 
-            # Stream the file contents
-            $fpointer = fopen($imageFile, "rb");
-            fpassthru($fpointer);
+                # Stream the temporary file to the client
+                streamRaw("/tmp/$tmpFile");
+            } else if (isset($_GET["raw"])) {
+                # Stream the entire image file to the client
+                streamRaw($imageFile);
+            }
 
             # Prevent further execution
             exit;
@@ -27,11 +43,29 @@
 ?>
 #!ipxe
 
+menu Boot method
+item --key m memory Boot from (M)emory
+item --key w windows Boot with (W)indows PE
+
+choose option && goto ${option}
+
 <?
     # TODO Check to make sure with realpath() that directory traversal is not taking place  
     
-    # TODO Prevent command injection
+    # TODO Prevent command injection (when using 7z)
+
+    # TODO This can also read files that are not .iso
 
     # Boot by storing the file in memory
-    echo "initrd $proto://$host/boot.php?image=" . $_GET["image"] . "&file";
 ?>
+
+:memory
+initrd <?= $proto ?>://<?= $host ?>/boot.php?image=<?= $image ?>&raw
+boot
+
+:windows
+kernel wimboot
+initrd <?= $proto ?>://<?= $host ?>/boot.php?image=<?= $image ?>&file=boot/bcd BCD
+initrd <?= $proto ?>://<?= $host ?>/boot.php?image=<?= $image ?>&file=boot/boot.sdi boot.sdi
+initrd <?= $proto ?>://<?= $host ?>/boot.php?image=<?= $image ?>&file=sources/boot.wim boot.wim
+boot
