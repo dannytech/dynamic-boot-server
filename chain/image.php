458ff710dbc1414f531890e9652486d367e4b986
Daniel Hellstern <dhellstern@outlook.com>
2021-04-06 08:28:46 +0000

    Clarified the meaning of each entry

--- chain/image.php
+++ chain/image.php
@@ -27,9 +27,9 @@
 ?>
 #!ipxe
 
-menu What kind of operating system is this?
-item --key w windows (W)indows 7 or beyond
-item --key d memory I (D)on't know
+menu Which operating system is this?
+item --key w windows (W)indows 7 or beyond - Boot with Windows PE
+item memory Unsure - Try to boot with a memory disk
 
 choose option && goto ${option}
 
